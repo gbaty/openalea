c8ff8e6117732f99a2a98ed1800095c01d6ad8a1
Guillaume Baty <guillaume.baty@inria.fr>
2014-09-30 14:34:31 +0200

    First draft to support OpenAleaLab applets

--- oalab/example/test_blender_ui.py
+++ oalab/example/test_blender_ui.py
@@ -1,8 +1,75 @@
 
-
+import weakref
 from openalea.vpltk.qt import QtGui, QtCore
 from openalea.oalab.service.applet import new_applet
 from openalea.oalab.gui.splitterui import SplittableUI
+from openalea.oalab.service.applet import get_applets
+from openalea.core.plugin import iter_plugins
+from openalea.oalab.session.session import Session
+
+class AppletContainer(QtGui.QTabWidget):
+    def __init__(self, parent=None):
+        QtGui.QTabWidget.__init__(self, None)
+#         self._layout = QtGui.QVBoxLayout(self)
+        self._cb_applets = QtGui.QComboBox()
+        self._cb_applets.currentIndexChanged.connect(self._on_current_applet_changed)
+
+        self._applets = {}
+        self._applet_plugins = []
+
+        self._container = QtGui.QWidget()
+        self._container_layout = QtGui.QVBoxLayout(self._container)
+
+#         self.setContentsMargins(0, 0, 0, 0)
+#         self._layout.setContentsMargins(0, 0, 0, 0)
+        self._container.setContentsMargins(0, 0, 0, 0)
+        self._container_layout.setContentsMargins(0, 0, 0, 0)
+
+        for plugin_class in iter_plugins('oalab.applet'):
+            if plugin_class.name in ['VtkViewer', 'OmeroClient', 'LineageViewer', ]:
+                self._applet_plugins.append(plugin_class)
+                self._cb_applets.addItem(plugin_class.alias)
+
+        self._container_layout.addWidget(self._cb_applets)
+
+        self.addTab(self._container, 'Tab1')
+
+#     def tabInserted(self, index):
+#         self.tabBar().setVisible(self.count() > 1)
+#
+#     def tabRemoved(self, index):
+#         self.tabBar().setVisible(self.count() > 1)
+
+    def add_applet(self, widget_class, name, *args, **kwds):
+        print 'add_applet', name, widget_class
+
+    def _on_current_applet_changed(self, idx):
+        if 0 <= idx <= len(self._applet_plugins):
+            plugin_class = self._applet_plugins[idx]
+
+            # clear view
+            for widget in self._applets.values():
+                widget.hide()
+
+            if plugin_class in self._applets:
+                widget = self._applets[plugin_class]
+                widget.show()
+            else:
+                plugin = plugin_class()
+                widget = plugin(self)()
+                self._applets[plugin_class] = widget
+                self._container_layout.insertWidget(0, widget)
+
+
+class OALabSplittableUi(SplittableUI):
+    def getPlaceHolder(self):
+        return AppletContainer()
+
+    def contextMenuEvent(self, event):
+        menu = QtGui.QMenu(self)
+        action = QtGui.QAction("Add tab", menu)
+        menu.addAction(action)
+        menu.exec_(event.globalPos())
 
 if __name__ == '__main__':
     instance = QtGui.QApplication.instance()
@@ -15,14 +82,13 @@ if __name__ == '__main__':
     for i in range(5):
         l.append(QtGui.QLabel(unicode(i)))
 
-    tab = QtGui.QTabWidget()
-    tab.addTab(l[1], '1')
-    tab.addTab(l[2], '2')
+
 
     mw = QtGui.QMainWindow()
-    splittable = SplittableUI(parent=mw)
-    splittable.setContentAt(0, l[0])
-    splittable.splitPane(tab, 0, QtCore.Qt.Horizontal, 0.8)
+
+    container = AppletContainer()
+    splittable = OALabSplittableUi(parent=mw)
+    splittable.setContentAt(0, container)
     mw.setCentralWidget(splittable)
     mw.show()
 
